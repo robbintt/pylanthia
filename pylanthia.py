d9ae8e240ae8ea0c7a48be98343d233c17ea7376
Trent Robbins <robbintt@gmail.com>
2018-10-14 00:00:00 -0700

    Broken, lxml is not used yet, need to reconsider line processing.

--- pylanthia.py
+++ pylanthia.py
@@ -19,6 +19,11 @@ import threading
 import time
 import itertools
 import re
+import datetime
+import logging
+import os
+
+import lxml.etree
 
 import urwid
 import urwid_readline
@@ -26,10 +31,6 @@ import urwid_readline
 from collections import deque
 from config import *
 
-import datetime
-import logging
-
-import os
 
 
 SCREEN_REFRESH_SPEED = 0.1 # how fast to redraw the screen from the buffer
@@ -81,13 +82,6 @@ def filter_lines(view_lines):
     for exclude in excludes:
         view_lines = [line for line in view_lines if line[0:len(exclude)] != exclude]
 
-    for line in view_lines:
-        logging.info("line is type"+str(type(line)))
-
-        for letter in line:
-            logging.info("letter is type"+str(type(letter)))
-
-
     # first lets just rip out the xml... later we will want to process it back into the stream
     # mostly we can use the xml just to update the state, if that's the case then if we miss
     # one then it's no proble, we just catch the next one... provided they are regular enough.
@@ -101,33 +95,79 @@ def filter_lines(view_lines):
         xml_line_segments = list()
         xml_free_line_part = b''
         xml_line_part = b''
+        ordered_parsed_line = list() # give a tuple of string, type
 
+        # ISSUE: i'm pretty sure this is dropping a letter off the first non-xml line segment (or more)
         # make a bunch of line segments
         # note that line is a bytes() type, indexing line[i] returns int
         # if we slice into it line[i:i+1] we get a bytes() type of length 1
         while i < len(line):
+
             if line[i:i+1] != b'<':
                 xml_free_line_part += line[i:i+1]
+
             else:
 
                 # increment until you get out of the xml tag or out of the line
                 while i < len(line) and line[i:i+1] != b'>':
                     xml_line_part += line[i:i+1]
                     i += 1
+
+                # toss the last b'>' on the end!
+                xml_line_part += line[i:i+1]
+
                 # store the xml part off
                 xml_line_segments.append(xml_line_part)
+                ordered_parsed_line.append(('xml', xml_line_part))
                 xml_line_part = b'' # reset the xml part
+
 
             # store xml free part off
             if len(xml_free_line_part) > 1:
                 xml_free_line_segments.append(xml_free_line_part)
+                ordered_parsed_line.append(('text', xml_free_line_part))
                 xml_free_line_part = b'' # reset the xml_free_line_part
 
             i += 1 # covers incrementing past the '>' and incrementing if not yet in a '<'
 
+
         # for now just join the xml free parts and only show those... we can process the xml elsewhere
 
-        xml_free_lines.append(b''.join(xml_free_line_segments))
+        '''
+        # https://lxml.de/tutorial.html
+        # if the xml cannot be parsed, we just want to catch it and decide what to do
+        try:
+            xml = [lxml.etree.XML(xml_line) for xml_line in xml_line_segments]
+            xml_tags = [x.tag for x in xml]
+            # just testing lxml tag parsing
+            if b'streamWindow' in xml_tags:
+                xml_free_lines.append(b'streamWindow skipped...')
+
+        except lxml.etree.XMLSyntaxError:
+            xml = list()  # no tags
+            # toss any failing XML onto the text stream for manual parsing?
+            # we can follow this approach even if we replace or wrap lxml with a manual parser
+            xml_free_lines.extend(xml_line_segments)
+        '''
+        # do stuff with the xml components of the line
+        op_line = ordered_parsed_line
+
+        if op_line:
+
+            if op_line[0][0] == 'xml':
+                if op_line[0][1].startswith(b'<prompt time="'):
+                    op_line.pop(0)
+
+
+        # strip the line back down to text
+        clean_line = [x[1].replace(b'&gt;', b'>') for x in op_line if x[0] == 'text']
+        xml_free_lines.append(b''.join(clean_line))
+
+        # send a hunk of xml so we can see what happened
+        xml_line = [x[1].replace(b'&gt;', b'>') for x in op_line if x[0] == 'xml']
+        xml_free_lines.append(b''.join(xml_line))
+
+
 
                         
     # just point it here for now so we don't have to change the return
