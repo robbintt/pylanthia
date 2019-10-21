166527cd942d1b9277eebd9bae8e049322c283e9
Trent Robbins <robbintt@gmail.com>
2019-10-21 22:29:18 +0000

    Break command processing into a library module.

---
+++ lib/command_processing.py
@@ -0,0 +1,52 @@
+'''
+'''
+import time
+import logging
+
+logging.getLogger(__name__)
+
+def process_command_queue(global_game_state, tcp_lines, gamesock, COMMAND_PROCESS_SPEED=0.3):
+    ''' process game commands from the submit queue
+
+    need some hotkey to dump the queue
+
+    don't process for n seconds if you got the response: "...wait 1 seconds."
+    '''
+    while True:
+
+        # this sleep throttles max command processing speed
+        time.sleep(COMMAND_PROCESS_SPEED)
+
+        if not global_game_state.command_queue.empty():
+            # maybe timestamped as its own output stream, so it can be turned off on certain windows
+            submitted_command = global_game_state.command_queue.get()
+
+            gamesock.sendall(submitted_command + b'\n')
+            tcp_lines.put(b'> ' + submitted_command)
+            logging.info(submitted_command)
+            global_game_state.command_history.put(submitted_command)
+            global_game_state.time_last_command = global_game_state.time
+
+            if submitted_command in [b'exit', b'quit']:
+                logging.info("quit triggered")
+                quit_event.set()
+
+            continue # ensure this whole queue is processed before the rt_command_queue
+
+        # process the rt_command_queue exactly as if a player had submitted again
+        # but always process the most recent player command_queue before the rt_command_queue
+        # this might get caught in a little submit race with the server, how to prevent?
+        # basically if there's a 1 second server time offset this could get submitted 100 times
+        # this often catches itself after submitting 2 commands
+        # since this puts things on the end of the queue, it gets things out of order
+        # it's not really intended for long strings of commands, but still it would be unexpected
+        # the user would not predict that things would start rotating like that...
+        # it's complicated, the commands should have an implied order in their data structure
+        # then the queue can be sorted again after putting an item on the queue
+        # more info to inform the `command data structure`
+        if not global_game_state.rt_command_queue.empty():
+            current_roundtime = int(global_game_state.roundtime - global_game_state.time)
+            if current_roundtime == 0:
+                global_game_state.command_queue.put(global_game_state.rt_command_queue.get())
+
+
--- pylanthia.py
+++ pylanthia.py
@@ -20,9 +20,9 @@ from lib import eaccess
 from lib import setup_game_connection
 from lib import get_tcp_lines
 from lib import text_processing
+from lib import command_processing
 from lib import urwid_ui
 
-COMMAND_PROCESS_SPEED = 0.3 # max speed that commands are submitted at
 MAX_IDLE_TIME = 60*60*2  # 60*60  # 60 minutes
 
 # set up logging into one place for now
@@ -63,51 +63,6 @@ class GlobalGameState:
                        'sw': False,
                        'se': False }
         return
-
-
-def process_command_queue(global_game_state, tcp_lines):
-    ''' process game commands from the submit queue
-
-    need some hotkey to dump the queue
-
-    don't process for n seconds if you got the response: "...wait 1 seconds."
-    '''
-    while True:
-
-        # this sleep throttles max command processing speed
-        time.sleep(COMMAND_PROCESS_SPEED)
-
-        if not global_game_state.command_queue.empty():
-            # maybe timestamped as its own output stream, so it can be turned off on certain windows
-            submitted_command = global_game_state.command_queue.get()
-
-            gamesock.sendall(submitted_command + b'\n')
-            tcp_lines.put(b'> ' + submitted_command)
-            logging.info(submitted_command)
-            global_game_state.command_history.put(submitted_command)
-            global_game_state.time_last_command = global_game_state.time
-
-            if submitted_command in [b'exit', b'quit']:
-                logging.info("quit triggered")
-                quit_event.set()
-
-            continue # ensure this whole queue is processed before the rt_command_queue
-
-        # process the rt_command_queue exactly as if a player had submitted again
-        # but always process the most recent player command_queue before the rt_command_queue
-        # this might get caught in a little submit race with the server, how to prevent?
-        # basically if there's a 1 second server time offset this could get submitted 100 times
-        # this often catches itself after submitting 2 commands
-        # since this puts things on the end of the queue, it gets things out of order
-        # it's not really intended for long strings of commands, but still it would be unexpected
-        # the user would not predict that things would start rotating like that...
-        # it's complicated, the commands should have an implied order in their data structure
-        # then the queue can be sorted again after putting an item on the queue
-        # more info to inform the `command data structure`
-        if not global_game_state.rt_command_queue.empty():
-            current_roundtime = int(global_game_state.roundtime - global_game_state.time)
-            if current_roundtime == 0:
-                global_game_state.command_queue.put(global_game_state.rt_command_queue.get())
 
 
 def gametime_incrementer(global_game_state):
@@ -163,7 +118,7 @@ if __name__ == '__main__':
     tcp_thread.daemon = True
     tcp_thread.start()
 
-    command_queue_thread = threading.Thread(target=process_command_queue, args=(global_game_state, tcp_lines))
+    command_queue_thread = threading.Thread(target=command_processing.process_command_queue, args=(global_game_state, tcp_lines, gamesock))
     command_queue_thread.daemon = True
     command_queue_thread.start()
 
