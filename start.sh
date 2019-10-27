321dc7b5aec4e5624617fd76fed4632d21df7e70
Trent Robbins <robbintt@gmail.com>
2019-10-27 21:08:25 +0000

    Extend lich startup pause back to 3 seconds.

--- start.sh
+++ start.sh
@@ -10,6 +10,6 @@ echo "Using lich path: $LICH_PATH"
 # must manually specify the rbw after upgrading
 ruby $LICH_PATH -s --game=DR -g dr.simutronics.net:4901 &
 
-sleep 2
+sleep 3 # 2 was too low
 
 cd /pylanthia && pipenv sync && pipenv run python3 pylanthia.py
