a4c2fc159a6a659d21b3890b806dde63d69aa72c
Trent Robbins <robbintt@gmail.com>
2019-11-09 11:17:12 -0800

    Update dockerfile, docker-compose, and entrypoint.sh to handle pipenv inside docker image.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ruby:latest 
+FROM ruby:latest as base
 
 # secrets still come from config.py which is mounted
 # FUTURE: secrets can come from .env when docker-compose entrypoint is hit...
@@ -9,6 +9,17 @@ RUN gem install sqlite3
 RUN apt-get update
 RUN apt-get install -y python3-pip
 RUN pip3 install pipenv
+
+# this gets used to setup pipenv, then the volume shadows it
+RUN mkdir /app
+WORKDIR /app
+COPY Pipfile Pipfile
+COPY Pipfile.lock Pipfile.lock
+# install into system? not sure if i want --system
+# --deploy fails the install if lockfile is out of date
+# https://pipenv-fork.readthedocs.io/en/latest/advanced.html#using-pipenv-for-deployments
+#RUN pipenv install --system --dev
+RUN pipenv sync
 
 # not working... i put this inside of start.sh instead
 #COPY deploy/openssl.cnf /etc/ssl/openssl.cnf
---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# specify a lower security level in modern ubuntu for old DH key
+# no idea why this isn't working in the dockerfile...
+ln -sf /app/deploy/openssl.cnf /etc/ssl/openssl.cnf
+
+# this manages the case in which lich is installed as .rb but not updated (to .rbw)
+LICH_PATH=$([[ -f "/lich/lich.rbw" ]] && echo "/lich/lich.rbw" || echo "/lich/lich.rb")
+echo "Using lich path: $LICH_PATH"
+
+# these need to come from config
+ruby $LICH_PATH -s --game=DR -g dr.simutronics.net:4901 &
+
+# is this even being caused by lich not booting up or is it a different network miss of some sort
+sleep 4 # 2 was too low, 3 is too low as well, 4 is too low... maybe i should wait for some input
+
+#cd /pylanthia && pipenv sync && pipenv run python3 pylanthia.py
+cd /app && pipenv run python3 pylanthia.py
